9783ec81e5fd2462b57488bc83e8cf24771e78b3
Gashun <gowagowa1990@gmail.com>
2021-04-05 18:46:15 +0300

    create to-do

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className="todo">
+			<div className="todo__sidebar">
+				
+			</div>
+			<div className="todo__content"></div>
+		</div>
+	);
+};
+
+export default App;
